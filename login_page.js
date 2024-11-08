08e0aee00de4a1a8969ce2a33862977efe2576f1
Awadhesh Kumar <140824899+Kumar-Awadhesh@users.noreply.github.com>
2024-11-08 17:04:29 +0530

    Add files via upload

--- login_page.js
+++ login_page.js
@@ -3,14 +3,19 @@ document.getElementById("form").addEventListener("submit", function(event){
 
     let phone = document.getElementById("phone").value;
 
+    if(!localStorage.getItem('userDetails')){
+        localStorage.setItem('userDetails', JSON.stringify([]));
+    }
+
     let getUser = localStorage.getItem('userDetails');
 
-    if(getUser !== null){
+    if(getUser){
         getUser = JSON.parse(getUser)
     }
     else{
         let getUser = [];
     }
+    console.log(getUser)
         
     let Users = getUser.find(user => user.phone === phone)
 
